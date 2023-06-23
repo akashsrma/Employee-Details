e5ae42958e1aa9bdb6408d0e9b8004ad7ba8ee41
akashsrma <akashsrma3695@gmail.com>
2023-06-23 15:31:21 +0530

    Employee Details

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+
+import "@styles/global.css";
+
+export const metadata = {
+  title: "Zen Time Potato",
+  description: "Zen Time Potato",
+};
+
+export default function RootLayout({ children }) {
+  return ( 
+    <html lang="en">
+      <body>{children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import  Employeeinputs from "../components/inputs/firstname/firstname"
+
+const Home = () => {
+  return(
+    <>
+    <Employeeinputs />
+    
+    </>
+  )
+};
+export default Home;
